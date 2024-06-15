e58039cabf6abab11f398ed9199b9d9025aa32a5
VincentLee363787493 <vincent@coinfloww.com>
2024-06-15 11:18:20 +0800

    commit index.html

--- js/script.js
+++ js/script.js
@@ -1,102 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const navLinks = document.querySelectorAll('.nav-menu a');
-//     const contentDiv = document.getElementById('content');
-//     const homeLink = document.querySelector('a[href="#home"]');
-//     const healthCareLink = document.querySelector('a[href="#healthcare"]');
-//     const browseLink = document.querySelector('a[href="#browse"]');
-//
-//     function setActiveLink(link) {
-//         navLinks.forEach(navLink => navLink.classList.remove('active'));
-//         link.classList.add('active');
-//     }
-//
-//     function loadContent(url, link, limit, page, category_level_one, category_level_two) {
-//         fetch(url)
-//             .then(response => response.text())
-//             .then(data => {
-//                 contentDiv.innerHTML = data;
-//                 setActiveLink(link);
-//                 if (link === homeLink) {
-//                     fetchArticles("homeArticles",20, 1, "", "");
-//                 } else {
-//                     fetchArticles("healthCareArticles",limit, page, category_level_one, category_level_two);
-//                 }
-//                 attachArticleClickHandlers(); // Attach click handlers to article links
-//             })
-//             .catch(error => console.error('Error loading content:', error));
-//     }
-//
-//     function fetchArticles(theme,limit, page, category_level_one, category_level_two) {
-//         let url = `http://localhost:8081/article?is_short=1&limit=${limit}&page=${page}`;
-//         if (category_level_one != "") {
-//             url = `${url}&category_level_one=${category_level_one}&category_level_two=${category_level_two}`;
-//         }
-//         console.log("url", url);
-//         fetch(url)
-//             .then(response => response.json())
-//             .then(articles => {
-//                 const articlesDiv = document.getElementById(theme);
-//                 articlesDiv.innerHTML = ''; // Clear old content
-//                 articles.data.forEach(article => {
-//                     const articleElement = document.createElement('article');
-//                     articleElement.classList.add('article'); // Add class for styling
-//                     articleElement.innerHTML = `
-//                         <div class="content">
-//                             <h3>
-//                                 <a href="http://localhost:8081/article/${article.id}" class="article-link" data-id="${article.id}">${article.title}</a>
-//                             </h3>
-//                             <p>${article.description}</p>
-//                         </div>
-//                         <a href="http://localhost:8081/article/${article.id}" class="article-link" data-id="${article.id}">
-//                             <img src="${article.cover_path}" alt="${article.title}">
-//                         </a>
-//                     `;
-//                     articlesDiv.appendChild(articleElement);
-//                 });
-//                 attachArticleClickHandlers(); // Attach click handlers to article links
-//             })
-//             .catch(error => console.error('Error fetching articles:', error));
-//     }
-//
-//     function fetchArticleContent(articleId) {
-//         fetch(`http://localhost:8081/article/${articleId}`)
-//             .then(response => response.json())
-//             .then(articleContent => {
-//                 contentDiv.innerHTML =articleContent.data.content; // Insert the article HTML content into the page
-//             })
-//             .catch(error => console.error('Error fetching article content:', error));
-//     }
-//
-//     function attachArticleClickHandlers() {
-//         const articleLinks = document.querySelectorAll('.article-link');
-//         articleLinks.forEach(link => {
-//             link.addEventListener('click', function (e) {
-//                 e.preventDefault();
-//                 const articleId = this.getAttribute('data-id');
-//                 fetchArticleContent(articleId);
-//             });
-//         });
-//     }
-//
-//     homeLink.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         loadContent('home.html', homeLink, 20, 1, "", "");
-//     });
-//
-//     healthCareLink.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         loadContent('health_care.html', healthCareLink, 20, 1, "Health", "Care");
-//     });
-//
-//     browseLink.addEventListener('click', function (e) {
-//         e.preventDefault();
-//         loadContent('browse.html', browseLink, 20, 1, "", "");
-//     });
-//
-//     // Load home.html content by default
-//     loadContent('home.html', homeLink, 20, 1, "", "");
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav-menu a');
     const contentDiv = document.getElementById('content');
@@ -115,12 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(data => {
                 contentDiv.innerHTML = data;
                 setActiveLink(link);
+                let theme=""
+                let level_one=""
+                let level_two=""
+                let limit_temp=0
+                let page_temp=0
                 if (link === homeLink) {
-                    fetchArticles("homeArticles", 20, 1, "", "");
-                } else {
-                    fetchArticles("healthCareArticles", limit, page, category_level_one, category_level_two);
+                    theme="homeArticles"
+                    limit_temp=20
+                    page_temp=1
+                } else if (link === healthCareLink){
+                    theme="healthCareArticles"
+                    level_one=category_level_one
+                    level_two=category_level_two
+                    limit_temp=limit
+                    page_temp=page
+                    attachToggleHandlers(theme);
                 }
-                attachArticleClickHandlers(); // Attach click handlers to article links
+                fetchArticles(theme, limit_temp, page_temp, level_one, level_two)
+
             })
             .catch(error => console.error('Error loading content:', error));
     }
@@ -130,7 +44,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if (category_level_one != "") {
             url = `${url}&category_level_one=${category_level_one}&category_level_two=${category_level_two}`;
         }
-        console.log("url", url);
         fetch(url)
             .then(response => response.json())
             .then(articles => {
@@ -152,8 +65,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     `;
                     articlesDiv.appendChild(articleElement);
                 });
-                attachArticleClickHandlers(); // Attach click handlers to article links
-                if (theme === "healthCareArticles") {
+                attachArticleClickHandlers(theme); // Attach click handlers to article links
+                if (theme !== "homeArticles") {
                     generatePagination(articles.totalCnt, limit, page, category_level_one, category_level_two);
                 }
             })
@@ -169,8 +82,14 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error fetching article content:', error));
     }
 
-    function attachArticleClickHandlers() {
-        const articleLinks = document.querySelectorAll('.article-link');
+    function attachArticleClickHandlers(theme) {
+        let selector=""
+        if (theme=="homeArticles"){
+            selector=".homeArticles .article-link"
+        }else if(theme=="healthCareArticles"){
+            selector=".healthCareArticles .article-link"
+        }
+        const articleLinks = document.querySelectorAll(selector);
         articleLinks.forEach(link => {
             link.addEventListener('click', function (e) {
                 e.preventDefault();
@@ -179,6 +98,30 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+    function attachToggleHandlers(theme) {
+        let level_one
+        let selector=""
+        if (theme=="adoptArticles"){
+            level_one=2
+            selector=".#healthCareContentTitle .toggle-checkbox"
+        }else if(theme=="healthCareArticles"){
+            level_one=1
+            selector="#healthCareContentTitle .toggle-checkbox"
+        }
+        const toggleLink = document.querySelector(selector);
+        toggleLink.addEventListener('change', function () {
+            if (toggleLink.checked) {
+                fetchArticles(theme, 5, 1, level_one, 1)
+                console.log('Cat mode activated!');
+            } else {
+                fetchArticles(theme, 5, 1, level_one, 2)
+                console.log('Dog mode activated!');
+            }
+        });
+
+    }
+
     function generatePagination(totalCnt, limit, currentPage, category_level_one, category_level_two) {
         const paginationDiv = document.getElementById('pagination');
         paginationDiv.innerHTML = ''; // Clear old pagination
@@ -261,6 +204,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         paginationDiv.appendChild(ul);
     }
+
     homeLink.addEventListener('click', function (e) {
         e.preventDefault();
         loadContent('home.html', homeLink, 20, 1, "", "");
@@ -268,7 +212,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     healthCareLink.addEventListener('click', function (e) {
         e.preventDefault();
-        loadContent('health_care.html', healthCareLink, 10, 1, 2, 2);
+        loadContent('health_care.html', healthCareLink, 5, 1, 1, 2);
     });
     browseLink.addEventListener('click', function (e) {
         e.preventDefault();
@@ -278,3 +222,4 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialize the home page by default
     loadContent('home.html', homeLink, 20, 1, "", "");
 });
+
